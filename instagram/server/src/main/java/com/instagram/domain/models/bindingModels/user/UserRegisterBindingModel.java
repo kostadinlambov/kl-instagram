577b5f7e200a09ca6bbb449af4e8b8fbff8c404a
Kostadin Lambov <valchak@abv.bg>
2019-07-24 23:51:29 +0200

    Server: register user

--- instagram/server/src/main/java/com/instagram/domain/models/bindingModels/user/UserRegisterBindingModel.java
+++ instagram/server/src/main/java/com/instagram/domain/models/bindingModels/user/UserRegisterBindingModel.java
@@ -20,10 +20,10 @@ public class UserRegisterBindingModel implements Serializable {
     private String confirmPassword;
     private String firstName;
     private String lastName;
-    private String address;
-    private String city;
-    private String profilePicUrl;
-    private String backgroundImageUrl;
+//    private String address;
+//    private String city;
+//    private String profilePicUrl;
+//    private String backgroundImageUrl;
 
     public UserRegisterBindingModel() {
     }
@@ -84,39 +84,39 @@ public class UserRegisterBindingModel implements Serializable {
         this.lastName = lastName;
     }
 
-    @NotNull(message = ValidationMessageConstants.USER_ADDRESS_REQUIRED_MESSAGE)
-    @Length(min = 1, message = ValidationMessageConstants.USER_ADDRESS_REQUIRED_MESSAGE)
-    public String getAddress() {
-        return this.address;
-    }
+//    @NotNull(message = ValidationMessageConstants.USER_ADDRESS_REQUIRED_MESSAGE)
+//    @Length(min = 1, message = ValidationMessageConstants.USER_ADDRESS_REQUIRED_MESSAGE)
+//    public String getAddress() {
+//        return this.address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    @NotNull(message = ValidationMessageConstants.USER_CITY_REQUIRED_MESSAGE)
+//    @Length(min = 1, message = ValidationMessageConstants.USER_CITY_REQUIRED_MESSAGE)
+//    public String getCity() {
+//        return this.city;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @NotNull(message = ValidationMessageConstants.USER_CITY_REQUIRED_MESSAGE)
-    @Length(min = 1, message = ValidationMessageConstants.USER_CITY_REQUIRED_MESSAGE)
-    public String getCity() {
-        return this.city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getProfilePicUrl() {
-        return this.profilePicUrl;
-    }
-
-    public void setProfilePicUrl(String profilePicUrl) {
-        this.profilePicUrl = profilePicUrl;
-    }
-
-    public String getBackgroundImageUrl() {
-        return this.backgroundImageUrl;
-    }
-
-    public void setBackgroundImageUrl(String backgroundImageUrl) {
-        this.backgroundImageUrl = backgroundImageUrl;
-    }
+//    public String getProfilePicUrl() {
+//        return this.profilePicUrl;
+//    }
+//
+//    public void setProfilePicUrl(String profilePicUrl) {
+//        this.profilePicUrl = profilePicUrl;
+//    }
+//
+//    public String getBackgroundImageUrl() {
+//        return this.backgroundImageUrl;
+//    }
+//
+//    public void setBackgroundImageUrl(String backgroundImageUrl) {
+//        this.backgroundImageUrl = backgroundImageUrl;
+//    }
 }
--- instagram/server/src/main/java/com/instagram/web/controllers/UserController.java
+++ instagram/server/src/main/java/com/instagram/web/controllers/UserController.java
@@ -1,12 +1,23 @@
 package com.instagram.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.instagram.domain.models.bindingModels.user.UserRegisterBindingModel;
 import com.instagram.services.UserService;
+import com.instagram.utils.responseHandler.exceptions.BadRequestException;
+import com.instagram.utils.responseHandler.successResponse.SuccessResponse;
 import com.instagram.validations.serviceValidation.services.UserValidationService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+import static com.instagram.utils.constants.ResponseMessageConstants.*;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -24,4 +35,32 @@ public class UserController {
         this.objectMapper = objectMapper;
         this.userValidationService = userValidationService;
     }
+
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<Object> registerUser(@RequestBody UserRegisterBindingModel userRegisterBindingModel) throws Exception {
+
+        if (!userValidationService.isValid(userRegisterBindingModel.getPassword(), userRegisterBindingModel.getConfirmPassword())) {
+            throw new BadRequestException(PASSWORDS_MISMATCH_ERROR_MESSAGE);
+        }
+
+//        if (!userValidationService.isValid(userRegisterBindingModel)) {
+//            throw new Exception(SERVER_ERROR_MESSAGE);
+//        }
+//
+//        UserServiceModel user = modelMapper.map(userRegisterBindingModel, UserServiceModel.class);
+//        UserCreateViewModel savedUser = this.userService.createUser(user);
+//
+//        SuccessResponse successResponse = successResponseBuilder(LocalDateTime.now(), SUCCESSFUL_REGISTER_MESSAGE, savedUser, true);
+//
+        return new ResponseEntity<>(this.objectMapper.writeValueAsString(userRegisterBindingModel), HttpStatus.OK);
+//        return new ResponseEntity<>(this.objectMapper.writeValueAsString(successResponse), HttpStatus.OK);
+
+//        return null;
+    }
+
+    private SuccessResponse successResponseBuilder(LocalDateTime timestamp, String message, Object payload, boolean success) {
+        return new SuccessResponse(timestamp, message, payload, success);
+    }
+
 }
